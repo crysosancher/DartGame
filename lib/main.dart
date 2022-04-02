fedc55db88939308539625c39d3373caf599fe65
crysosancher <vibhupandey.vp@gmail.com>
2022-04-02 15:48:46 +0530

    font-added

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,25 @@ void main() {
   runApp(MaterialApp(
     home:Scaffold(
       appBar: AppBar(
-        title: Text('my first app'),
+        title: Text('Virat Pandey'),
         centerTitle: true,
+        backgroundColor: Colors.blueGrey,
       ),
       body:Center(
-          child:Text("Hello Ninjas")
+          child:Text("Observe⥂Think⥂Implement "
+          ,style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[850],
+              fontFamily: 'IndieFlower',
+
+            ),)
       ),
       floatingActionButton: FloatingActionButton(
-        child:Text('click'),
+        child:Text('Noob'),
         onPressed: (){},
+        backgroundColor: Colors.blueGrey,
       ),
     ),
 
